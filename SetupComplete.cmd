25a0d132d682ee2699201678b1b80a53133c0548
alive-one <88646366+alive-one@users.noreply.github.com>
2022-03-02 16:50:03 +0800

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -1,5 +1,8 @@
-rem | Remove no longer neccessary answers file
+rem | Get rid of password expires issue, for password change alert will stop script execution
+wmic path Win32_UserAccount where (name="user") SET PasswordExpires=FALSE
+ 
+rem | Delete Unattend.xml whic is no longer neccessary
 del %SYSTEMDRIVE%\Unattend.xml
-
-rem | Create regedit key which will start autoren.bat on logon
-Reg Add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce /V RenamePC /D "cmd /C C:\Windows\Setup\Scripts\autoren.bat" /F
+ 
+rem | Create Regedit key which set system to copy and execute autoren.bat after autologon
+Reg Add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce /V RenamePC /D "cmd /C C:\Windows\Setup\Scripts\cp-autoren.bat" /F
